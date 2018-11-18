ed8dfbc861844c0630de4135649741cedcc97cce
Muhammad Haseeb <haseeb.emailbox@gmail.com>
2018-11-18 19:16:52 +0500

    Setting up

---
+++ readme.md
@@ -0,0 +1,20 @@
+# quixel
+
+A happy light weight framework for semantic text content analysis.
+
+## Installation
+
+Installation is simplest with pip:
+
+    pip install quixel
+    
+## Contributing
+
+Feel free to submit pull requests to me.
+
+## Authors
+
+* **Muhammad Haseeb** - *Initial work* - [Muhammad Haseeb](https://github.com/iam-mhaseeb)
+
+## Licensing
+The pycloudy library is [MIT Licenced](LICENSE). Feel free to use commercially of personally.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import  io
+from setuptools import setup
+
+with io.open('readme.md', encoding='utf_8') as fp:
+    readme = fp.read()
+
+setup(name='quixel',
+      version='1.0',
+      description='A happy light weight framework for semantic text content analysis.',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      keywords='python, text analysis, nlp',
+      url='https://github.com/iam-mhaseeb/quixel',
+      author='Muhammad Haseeb',
+      author_email='haseeb.emailbox@gmail.com',
+      license='MIT',
+      packages=['quixel'],
+      zip_safe=False,
+      include_package_data=True,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
